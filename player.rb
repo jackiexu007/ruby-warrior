33a05d9a909875d7915a47b70d905e2f98b2585a
Jackie Xu <jackieeexu007@gmail.com>
2018-06-28 10:05:17 -0700

    level8

--- player.rb
+++ player.rb
@@ -1,34 +1,14 @@
 class Player
   def play_turn(warrior)
         @health = 20 if @health.nil?
-        if warrior.feel(:forward).wall?
-		warrior.pivot!
-	elsif warrior_need_rest?(warrior)
-		if warrior_able_to_rest?(warrior)
-			warrior.rest!
-		else
-			warrior.walk!(:backward)
-		end
-        else
-		if warrior.feel.empty?
-                	warrior.walk!
-		else
-			warrior.attack!
-		end
-        end
-                @health = warrior.health
-  end
-  def warrior_under_attack?(warrior)
-        @health > warrior.health
-  end
-  def rest_not_enough?(warrior)
-	@health < 15 && warrior_under_attack?(warrior)!=TRUE
-  end
-  def warrior_need_rest?(warrior)
-	rest_not_enough?(warrior)  ||  @health < 10
-  end
-  def warrior_able_to_rest?(warrior)
-	warrior.feel(:backward).empty?!=TRUE && warrior_under_attack?(warrior)!=TRUE
+	if warrior.feel.captive?
+		warrior.rescue!	
+	elsif warrior.look[1].enemy?
+		warrior.shoot!
+	else
+		warrior.walk!
+	end
+		@health = warrior.health
   end
 end
 
