afc3a71756e2907f1357ac9f943403d366942292
Jackie Xu <jackieeexu007@gmail.com>
2018-06-28 00:52:02 -0700

    level7

---
+++ level7.rb
@@ -0,0 +1,34 @@
+class Player
+  def play_turn(warrior)
+        @health = 20 if @health.nil?
+        if warrior.feel(:forward).wall?
+		warrior.pivot!
+	elsif warrior_need_rest?(warrior)
+		if warrior_able_to_rest?(warrior)
+			warrior.rest!
+		else
+			warrior.walk!(:backward)
+		end
+        else
+		if warrior.feel.empty?
+                	warrior.walk!
+		else
+			warrior.attack!
+		end
+        end
+                @health = warrior.health
+  end
+  def warrior_under_attack?(warrior)
+        @health > warrior.health
+  end
+  def rest_not_enough?(warrior)
+	@health < 15 && warrior_under_attack?(warrior)!=TRUE
+  end
+  def warrior_need_rest?(warrior)
+	rest_not_enough?(warrior)  ||  @health < 10
+  end
+  def warrior_able_to_rest?(warrior)
+	warrior.feel(:backward).empty?!=TRUE && warrior_under_attack?(warrior)!=TRUE
+  end
+end
+
